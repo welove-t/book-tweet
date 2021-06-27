da00a43c4da1a66f321daf02a243f51e32329956
welove-t <tlove.tky@gmail.com>
2021-06-28 08:03:55 +0900

    📝 利用規約ページ作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { FieldsetHTMLAttributes } from 'react';
+import React from 'react';
 import Header from '../components/lp/Header';
 import Hero from '../components/lp/Hero';
 import Features from '../components/lp/Features';
