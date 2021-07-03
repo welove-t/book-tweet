ced8200dac6783e1ff1ce824f3d819ebfd776fc6
welove-t <tlove.tky@gmail.com>
2021-07-03 16:05:05 +0900

    💄 ライブラリ画面作成

--- components/lp/Header.tsx
+++ components/lp/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
           <a className="font-bold text-2xl text-white">LOGO</a>
         </Link>
         <span className="flex-1"></span>
-        <Link href="/">
+        <Link href="/library">
           <a className="text-white border border-white rounded-md py-2 px-4">
             ログイン
           </a>
