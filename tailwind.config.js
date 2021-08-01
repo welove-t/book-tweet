680f70a93e221f1abca4486789caf9e4cabb43d1
welove-t <tlove.tky@gmail.com>
2021-08-01 13:06:56 +0900

    💄 ログインモーダルの作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitter: '#00ACEE',
+      },
+    },
     container: {
       center: true,
       padding: {
