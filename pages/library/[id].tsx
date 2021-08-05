62fe92bb84e22c3a3efb58b6e83b0afd04bfbbd1
welove-t <tlove.tky@gmail.com>
2021-08-06 08:06:20 +0900

    💄 画像生成UIの構築（canvas導入）

--- pages/library/[id].tsx
+++ pages/library/[id].tsx
@@ -20,6 +20,13 @@ const BookDetail = () => {
   // 本削除時のダイアログ表示ステイト
   const [isOpen, setIsOpen] = useState(false);
 
+  // canvas用
+  const [bgColor, setBgColor] = useState<string>('#888888');
+  const [foColor, setFoColor] = useState<string>('#000000');
+  const [png, setPng] = useState<string | null>(null);
+  const width = 350;
+  const height = 255;
+
   const closeModal = () => {
     setIsOpen(false);
   };
@@ -56,6 +63,27 @@ const BookDetail = () => {
   useEffect(() => {
     getBookData();
   }, []);
+
+  useEffect(() => {
+    const canvasElem = document.createElement('canvas');
+    canvasElem.width = width;
+    canvasElem.height = height;
+
+    const ctx = canvasElem && canvasElem.getContext('2d');
+    if (!canvasElem || !ctx) return;
+
+    // draw
+
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.font = '30px Hiragino Maru Gothic Pro';
+    ctx.fillStyle = foColor;
+    ctx.fillText(bookData.comment, width / 6, height / 2);
+
+    setPng(canvasElem.toDataURL());
+  }, [bgColor, foColor, bookData.comment]);
 
   // 本の削除
   const deleteBook = () => {
@@ -146,6 +174,41 @@ const BookDetail = () => {
             </a>
           </div>
         </form>
+        <div>
+          <h3>画像生成</h3>
+          <h4>背景色</h4>
+          {['#f00', '#0f0', '#00f'].map((color) => (
+            <button
+              key={color}
+              style={{ background: color }}
+              onClick={() => setBgColor(color)}
+            >
+              {color}
+            </button>
+          ))}
+          <h4>文字色</h4>
+          {['#f00', '#0f0', '#00f'].map((color) => (
+            <button
+              key={color}
+              style={{ color }}
+              onClick={() => setFoColor(color)}
+            >
+              {color}
+            </button>
+          ))}
+          <h4>生成</h4>
+          {png && (
+            <div className="text-center">
+              <Image
+                alt="icon"
+                src={png}
+                height={height}
+                width={width}
+                className="rounded-lg"
+              />
+            </div>
+          )}
+        </div>
       </div>
       <Toaster />
       <Transition appear show={isOpen} as={Fragment}>
